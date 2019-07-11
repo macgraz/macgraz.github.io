33b6da66345636077138c26c6e377987e4d83645
toomuch-at <41962632+toomuch-at@users.noreply.github.com>
2019-07-11 15:40:35 +0200

    change

--- all.js
+++ all.js
@@ -31,6 +31,15 @@ if (is_touch_device()) {
     });
 }
 
+/* iOS re-orientation fix */
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+    /* iOS hides Safari address bar */
+    window.addEventListener("load",function() {
+        setTimeout(function() {
+            window.scrollTo(0, 1);
+        }, 1000);
+    });
+}
 
 //LOADER
 

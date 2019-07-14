03bf66175055698746c121553d2e1528168c7b30
toomuch-at <41962632+toomuch-at@users.noreply.github.com>
2019-07-14 12:20:24 +0200

    change

--- all.js
+++ all.js
@@ -37,10 +37,15 @@ $(".overlay").removeClass("no-js")
 $("#no-js").addClass("hidden")
 
 // bg change
-var images = ['bg-01.jpeg', 'bg-02.jpg', 'bg-03.jpg', 'bg-04.jpg', 'bg-05.jpg', 'bg-06.jpg', 'bg-07.jpg', 'bg-08.jpg', 'bg-09.jpg', 'bg-10.jpg', 'bg-11.jpg', 'bg-12.jpg'];
-$("div.bg").css({
-    'background-image': 'url(img/bg/' + images[Math.floor(Math.random() * images.length)] + ')'
+setTimeout(function () {
+    document.querySelector("body").classList.add("img")
+}, 3000)
+
+setTimeout(function () {
+    var images = ['bg-01.jpeg', 'bg-02.jpg', 'bg-03.jpg', 'bg-04.jpg', 'bg-05.jpg', 'bg-06.jpg', 'bg-07.jpg', 'bg-08.jpg', 'bg-09.jpg', 'bg-10.jpg', 'bg-11.jpg', 'bg-12.jpg'];
+$("body.img").css({'background-image': 'url(img/bg/' + images[Math.floor(Math.random() * images.length)] + ')'
 });
+}, 4000)
 
 // after 5 seconds
 setTimeout(function () {
@@ -193,7 +198,8 @@ $("#alert .button.grey").on("click touch", function () {
         $("#alert").removeClass("visible");
     }, 100);
     setTimeout(function () {
-        $("div.bg").addClass("hidden");
+        $("body").removeClass("img");
+        $("body").css("background-image", "none");
         $("section").removeClass("visible");
     }, 2000);
     setTimeout(function () {

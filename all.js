489ba71ecc0440c302c5b019e04c1a8d52dc1cb1
toomuch-at <41962632+toomuch-at@users.noreply.github.com>
2019-07-14 12:23:57 +0200

    change

--- all.js
+++ all.js
@@ -38,14 +38,10 @@ $("#no-js").addClass("hidden")
 
 // bg change
 setTimeout(function () {
-    document.querySelector("body").classList.add("img")
-}, 3000)
-
-setTimeout(function () {
     var images = ['bg-01.jpeg', 'bg-02.jpg', 'bg-03.jpg', 'bg-04.jpg', 'bg-05.jpg', 'bg-06.jpg', 'bg-07.jpg', 'bg-08.jpg', 'bg-09.jpg', 'bg-10.jpg', 'bg-11.jpg', 'bg-12.jpg'];
-$("body.img").css({'background-image': 'url(img/bg/' + images[Math.floor(Math.random() * images.length)] + ')'
+$("body").css({'background-image': 'url(img/bg/' + images[Math.floor(Math.random() * images.length)] + ')'
 });
-}, 4000)
+}, 2000)
 
 // after 5 seconds
 setTimeout(function () {
@@ -198,7 +194,6 @@ $("#alert .button.grey").on("click touch", function () {
         $("#alert").removeClass("visible");
     }, 100);
     setTimeout(function () {
-        $("body").removeClass("img");
         $("body").css("background-image", "none");
         $("section").removeClass("visible");
     }, 2000);

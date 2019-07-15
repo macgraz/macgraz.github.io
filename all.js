086dbc4717bb0b00dd40c384fe172b0a6f80fa1e
toomuch-at <41962632+toomuch-at@users.noreply.github.com>
2019-07-15 14:51:35 +0200

    change

--- all.js
+++ all.js
@@ -25,7 +25,7 @@ if (is_touch_device()) {
     var images = ['bg-01.jpeg', 'bg-02.jpg', 'bg-03.jpg', 'bg-04.jpg', 'bg-05.jpg', 'bg-06.jpg', 'bg-07.jpg', 'bg-08.jpg', 'bg-09.jpg', 'bg-10.jpg', 'bg-11.jpg', 'bg-12.jpg'];
     $("div.bg").css({'background-image': 'url(img/bg/' + images[Math.floor(Math.random() * images.length)] + ')'
     });
-    
+
     $("body").mouseover(function () {
         $("section").addClass("visible");
     });
@@ -44,6 +44,10 @@ $("#no-js").addClass("hidden")
 setTimeout(function () {
     document.querySelector("div.loader").classList.add("hidden")
 }, 5000)
+
+setTimeout(function () {
+    document.querySelector("div.main").classList.remove("hidden")
+}, 5500)
 
 setTimeout(function () {
     document.querySelector("#info").classList.add("visible")
